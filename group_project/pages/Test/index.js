2b23bbed6da9aff0cb9f1e9f5686ca55cbf4c129
Smileyright <alexplaysoccer880@gmail.com>
2025-04-13 20:08:27 -0400

    Start of project

---
+++ group_project/pages/Test/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function testing() {
+    return(
+    <h1>HIII</h1>
+    )
+}
+
+export default testing;
---
+++ group_project/pages/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Link from "next/link";
+
+function Home() {
+  return (
+    <Link href="/Test">.</Link>
+  )
+}
+
+export default Home;
