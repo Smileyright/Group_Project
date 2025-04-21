c9324ba66b7c3575c6d2b2fe81bdfe5d806972c4
Smileyright <alexplaysoccer880@gmail.com>
2025-04-21 14:47:47 -0400

    Navbar + simple about

--- group_project/components/Navbar.js
+++ group_project/components/Navbar.js
@@ -6,6 +6,7 @@ const Navbar = () => {
     return (
         <div className={styles.navcontainer}>
 
+<<<<<<< Updated upstream
           <div className={styles.navLogo}>
             <img src="/images/sqare.jpg" width="100" height="100" alt="Logo" />
           </div>
@@ -17,6 +18,17 @@ const Navbar = () => {
             <li><Link href="/Gallery">Gallery</Link></li>
             <li><Link href="/Contact">Contact</Link></li>
           </ul>
+=======
+            <div className={styles.navlinks}>
+                <ul>
+                    <li><Link href="/">Home</Link></li>
+                    <li><Link href="/Artists">Artists</Link></li>
+                    <li><Link href="/Gallery">Gallery</Link></li>
+                    <li><Link href="/Contact">Contact</Link></li>
+                    <li><Link href="/About">About</Link></li>
+                </ul>
+            </div>
+>>>>>>> Stashed changes
         </div>
       </div>
     )
--- group_project/pages/About/index.js
+++ group_project/pages/About/index.js
@@ -1,12 +1,21 @@
 import React from "react";
 import FooterComponent from "@/components/Footer";
+import HeaderComponent from "@/components/Header";
+import Navbar from "@/components/Navbar";
+import styles from "@/styles/about.module.css";
 
 
 function AboutPage() {
     return (
-        <div>
+        <div className={styles.container}>
+            <div className={styles.content}>
+            <HeaderComponent />
+            <Navbar />
             <h1>This is the about page</h1>
+            </div>
+            <div className={styles.footer}>
             <FooterComponent />
+            </div>
         </div>
     )
 }
